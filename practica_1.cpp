6c978f91f2b4a495ef3cd366360511cc1c5c7197
Alejandro Estornell <aaesalamanca@gmail.com>
2025-11-11 16:06:47 +0100

    feat: Practice 1 solved (#4)

---
+++ practica_1.cpp
@@ -0,0 +1,17 @@
+/*********************************
+ * NOMBRE: #Alejandro Antonio#
+ * PRIMER APELLIDO: #Estornell#
+ * SEGUNDO APELLIDO: #Salamanca#
+ * DNI: #09067127#
+ * EMAIL: #aaesalamanca@gmail.com#
+ *********************************/
+
+#include <stdio.h>
+
+int main() {
+  printf("Alejandro Antonio\n");
+  printf("Estornell\n");
+  printf("Salamanca\n");
+  printf("09067127\n");
+  printf("aaesalamanca@gmail.com\n");
+}
