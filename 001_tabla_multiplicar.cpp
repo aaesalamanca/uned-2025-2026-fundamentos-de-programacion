f6051d15feffe1808390e74416aeea5c752af719
aaesalamanca <aaesalamanca@gmail.com>
2025-11-29 17:57:25 +0100

    feat: Solve exercise 1 - Multiplication table

---
+++ 001_tabla_multiplicar.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+  int number, product;
+
+  printf("¿Número?: ");
+  scanf("%d", &number);
+
+  printf("\n");
+  printf("Tabla de multiplicar por %d\n", number);
+  printf("==========================\n");
+
+  for (int i = 1; i < 11; i++) {
+    product = number * i;
+    printf("\t%d x %2d = %3d\n", number, i, product);
+  }
+}
